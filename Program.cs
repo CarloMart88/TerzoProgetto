20b59c6aa266a1860c8c2659838ebfbd63ac3f9c
CarloMart88 <carlo.martino1988@gmail.com>
2025-10-28 12:27:07 +0100

    add new paragraphs

--- Program.cs
+++ Program.cs
@@ -14,5 +14,8 @@ class Program
     Console.WriteLine("\n");
     Console.WriteLine("Fueled by passion for your craft,  you give chase...");
     Console.WriteLine("----------------------------------------------------");
+    Console.WriteLine("Through winding alleys and crowded streets, you \n pursue the pizza pilferer. Finally, the Thief is\n cornered in a dead-end alley. It's time to recover \n your stolen slice!");
+    Console.WriteLine("----------------------------------------------------");
+    Console.WriteLine("                    🍕 FIGHT !🍕");
   }
 }
